a7c4fa5f548627723375c34f7fcbd75c8826a0bf
LiuVuitton <liulihao92@gmail.com>
2025-12-25 23:33:08 +0100

    File folder restructuring

---
+++ interpreter/build/CMakeFiles/lox.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lil9zrh/clox/interpreter/main.cpp" "CMakeFiles/lox.dir/main.cpp.o" "gcc" "CMakeFiles/lox.dir/main.cpp.o.d"
+  "/home/lil9zrh/clox/interpreter/src/ast_printer.cpp" "CMakeFiles/lox.dir/src/ast_printer.cpp.o" "gcc" "CMakeFiles/lox.dir/src/ast_printer.cpp.o.d"
+  "/home/lil9zrh/clox/interpreter/src/environment.cpp" "CMakeFiles/lox.dir/src/environment.cpp.o" "gcc" "CMakeFiles/lox.dir/src/environment.cpp.o.d"
+  "/home/lil9zrh/clox/interpreter/src/expr.cpp" "CMakeFiles/lox.dir/src/expr.cpp.o" "gcc" "CMakeFiles/lox.dir/src/expr.cpp.o.d"
+  "/home/lil9zrh/clox/interpreter/src/function_return.cpp" "CMakeFiles/lox.dir/src/function_return.cpp.o" "gcc" "CMakeFiles/lox.dir/src/function_return.cpp.o.d"
+  "/home/lil9zrh/clox/interpreter/src/interpreter.cpp" "CMakeFiles/lox.dir/src/interpreter.cpp.o" "gcc" "CMakeFiles/lox.dir/src/interpreter.cpp.o.d"
+  "/home/lil9zrh/clox/interpreter/src/lox.cpp" "CMakeFiles/lox.dir/src/lox.cpp.o" "gcc" "CMakeFiles/lox.dir/src/lox.cpp.o.d"
+  "/home/lil9zrh/clox/interpreter/src/lox_callable.cpp" "CMakeFiles/lox.dir/src/lox_callable.cpp.o" "gcc" "CMakeFiles/lox.dir/src/lox_callable.cpp.o.d"
+  "/home/lil9zrh/clox/interpreter/src/lox_class.cpp" "CMakeFiles/lox.dir/src/lox_class.cpp.o" "gcc" "CMakeFiles/lox.dir/src/lox_class.cpp.o.d"
+  "/home/lil9zrh/clox/interpreter/src/lox_function.cpp" "CMakeFiles/lox.dir/src/lox_function.cpp.o" "gcc" "CMakeFiles/lox.dir/src/lox_function.cpp.o.d"
+  "/home/lil9zrh/clox/interpreter/src/lox_instance.cpp" "CMakeFiles/lox.dir/src/lox_instance.cpp.o" "gcc" "CMakeFiles/lox.dir/src/lox_instance.cpp.o.d"
+  "/home/lil9zrh/clox/interpreter/src/native_function.cpp" "CMakeFiles/lox.dir/src/native_function.cpp.o" "gcc" "CMakeFiles/lox.dir/src/native_function.cpp.o.d"
+  "/home/lil9zrh/clox/interpreter/src/parser.cpp" "CMakeFiles/lox.dir/src/parser.cpp.o" "gcc" "CMakeFiles/lox.dir/src/parser.cpp.o.d"
+  "/home/lil9zrh/clox/interpreter/src/resolver.cpp" "CMakeFiles/lox.dir/src/resolver.cpp.o" "gcc" "CMakeFiles/lox.dir/src/resolver.cpp.o.d"
+  "/home/lil9zrh/clox/interpreter/src/runtime_error.cpp" "CMakeFiles/lox.dir/src/runtime_error.cpp.o" "gcc" "CMakeFiles/lox.dir/src/runtime_error.cpp.o.d"
+  "/home/lil9zrh/clox/interpreter/src/scanner.cpp" "CMakeFiles/lox.dir/src/scanner.cpp.o" "gcc" "CMakeFiles/lox.dir/src/scanner.cpp.o.d"
+  "/home/lil9zrh/clox/interpreter/src/stmt.cpp" "CMakeFiles/lox.dir/src/stmt.cpp.o" "gcc" "CMakeFiles/lox.dir/src/stmt.cpp.o.d"
+  "/home/lil9zrh/clox/interpreter/src/token.cpp" "CMakeFiles/lox.dir/src/token.cpp.o" "gcc" "CMakeFiles/lox.dir/src/token.cpp.o.d"
+  "/home/lil9zrh/clox/interpreter/src/token_type.cpp" "CMakeFiles/lox.dir/src/token_type.cpp.o" "gcc" "CMakeFiles/lox.dir/src/token_type.cpp.o.d"
+  "/home/lil9zrh/clox/interpreter/src/utility.cpp" "CMakeFiles/lox.dir/src/utility.cpp.o" "gcc" "CMakeFiles/lox.dir/src/utility.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ interpreter/build/CMakeFiles/lox.dir/src/function_return.cpp.o.d
@@ -0,0 +1,20 @@
+CMakeFiles/lox.dir/src/function_return.cpp.o: \
+ /home/lil9zrh/clox/interpreter/src/function_return.cpp \
+ /usr/include/stdc-predef.h \
+ /home/lil9zrh/clox/interpreter/include/function_return.h \
+ /usr/include/c++/13/any /usr/include/c++/13/initializer_list \
+ /usr/include/x86_64-linux-gnu/c++/13/bits/c++config.h \
+ /usr/include/x86_64-linux-gnu/c++/13/bits/os_defines.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/c++/13/bits/cpu_defines.h \
+ /usr/include/c++/13/pstl/pstl_config.h /usr/include/c++/13/typeinfo \
+ /usr/include/c++/13/bits/exception.h \
+ /usr/include/c++/13/bits/hash_bytes.h /usr/include/c++/13/new \
+ /usr/include/c++/13/type_traits /usr/include/c++/13/bits/utility.h \
+ /usr/include/c++/13/bits/move.h
